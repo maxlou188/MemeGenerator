5203f1dee6085dd7c371f340003e2a89c9ff9176
dudeman123 <maxlou188@gmail.com>
2022-06-20 01:21:18 -0700

    all this stuff

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
 import Navbar from './components/Navbar';
 import Meme from './components/Meme';
 import React from 'react';
-import Box from './components/Box';
-import boxesArray from './boxes';
 
 function App(props) {
-  const [boxes, setBoxes] = React.useState(boxesArray);
-
-  function toggle(id) {
-    setBoxes(prevBoxes => {
-      return prevBoxes.map((box) => {
-        return box.id === id ? {...box, on: !box.on} : box;
-      });
-    });
-  };
-
-  const boxesElements = boxes.map(box => 
-    <Box
-      isOn={box.on}
-      isDarkMode={props.darkMode}
-      handleClick={toggle}
-      id={box.id}
-    />);
 
   return (
     <div className="container">
-      {/* <Navbar />
-      <Meme /> */}
-      {boxesElements}
+      <Navbar />
+      <Meme />
     </div>
   );
 }
